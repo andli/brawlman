9a9796faba1fd471b1575979f44ea039fc046333
Andreas Limber <andreas.limber@gmail.com>
2025-03-07 15:54:10 +0100

    tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// filepath: /Users/an-ehs/repos/brawlman/tailwind.config.js
+module.exports = {
+  // Comment out or remove the content property to disable PurgeCSS
+  // content: [
+  //   './pages/**/*.{js,ts,jsx,tsx}',
+  //   './components/**/*.{js,ts,jsx,tsx}',
+  // ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
